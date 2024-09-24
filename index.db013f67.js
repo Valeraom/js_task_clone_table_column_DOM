74c91c6a374a932ef07a517c7b0f47334fb6eb3e
Valerii Omelnyk <valeraaomelnik@gmail.com>
2024-09-24 17:14:26 +0300

    make build

---
+++ index.db013f67.js
@@ -0,0 +1,2 @@
+var r=document.querySelector("table"),e=!0,l=!1,t=void 0;try{for(var o,a=r.rows[Symbol.iterator]();!(e=(o=a.next()).done);e=!0){var n=o.value.cells;n[3].after(n[1].cloneNode(!0))}}catch(r){l=!0,t=r}finally{try{e||null==a.return||a.return()}finally{if(l)throw t}}
+//# sourceMappingURL=index.db013f67.js.map
